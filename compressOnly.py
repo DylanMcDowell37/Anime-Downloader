11d06c7aaa5ecdcaa0ae558e1146ffc50a3270d8
Dylan McDowell <mcdowelldylan52@gmail.com>
2023-04-08 22:19:15 -0700

    Changes for Jellyfin Server
    This is changed to run on Linux server

--- compressOnly.py
+++ compressOnly.py
@@ -11,7 +11,7 @@ def compress_video(video_full_path, output_file_name, target_size):
     # Video duration, in s.
     duration = float(probe['format']['duration'])
     # Audio bitrate, in bps.
-    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), {}).get('bit_rate', '0'))
     # Target total bitrate, in bps.
     target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
 
@@ -32,26 +32,14 @@ def compress_video(video_full_path, output_file_name, target_size):
     ffmpeg.output(i, output_file_name,
                   **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
                   ).overwrite_output().run()
-
-def search_for_file_path():
-    # Get the list of directories in the current working directory
-    dirs = [d for d in os.listdir('.') if os.path.isdir(d)]
-    # Sort the directories by creation time (newest first)
-    dirs.sort(key=lambda x: os.path.getctime(x), reverse=True)
-    # Return the newest directory
-    if dirs:
-        return os.path.abspath(dirs[0])
-
-anime = input('anime: ')
-file_path_variable = search_for_file_path()
-if file_path_variable:
-    print("Newest directory: ", file_path_variable)
-    for files in os.listdir(file_path_variable):
-        videoin = os.path.join(file_path_variable, files)
-        videoout = os.path.join(file_path_variable, "compressed" + files.replace('.ts', '.mp4'))
-        compress_video(videoin, videoout, 50 * 1000)
-        os.remove(videoin)
+    
+dir = input("Enter Directory Name: ")
+file_path_variable = "/media/"+dir
+print("Directory: ", file_path_variable)
+for files in os.listdir(file_path_variable):
+    videoin = os.path.join(file_path_variable, files)
+    videoout = os.path.join(file_path_variable, "compressed" + files.replace('.ts', '.mp4'))
+    compress_video(videoin, videoout, 50 * 1000)
+    os.remove(videoin)
 else:
     print("No directories found.")
-
-
--- show.py
+++ show.py
@@ -1,65 +1,136 @@
-import os
-import subprocess
+import requests
 import time
+import re
+import os
+import ffmpeg
+import rarfile
+import fnmatch
+import subprocess
 
-# Set the target size for the compressed video, in kilobits per second.
-target_size = 50 * 1000
-
-# Set the path to the directory where the downloaded files will be saved.
-download_directory = "/path/to/download/directory"
-
-# Set the path to the directory where the compressed files will be saved.
-compress_directory = "/path/to/compress/directory"
-
-# Define a function to compress a video file.
-def compress_video(video_full_path, output_file_name):
+def compress_video(video_full_path, output_file_name, target_size):
     # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
     min_audio_bitrate = 32000
     max_audio_bitrate = 256000
 
-    probe = subprocess.check_output(["ffprobe", "-v", "error", "-show_entries", "format=duration,bit_rate", "-of", "default=noprint_wrappers=1:nokey=1", video_full_path]).decode("utf-8")
-    probe = probe.strip().split("\n")
-    duration = float(probe[0])
-    audio_bitrate = float(probe[1])
-
+    probe = ffmpeg.probe(video_full_path)
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), {}).get('bit_rate', '0'))
+    # Target total bitrate, in bps.
     target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
 
+    # Target audio bitrate, in bps
     if 10 * audio_bitrate > target_total_bitrate:
         audio_bitrate = target_total_bitrate / 10
         if audio_bitrate < min_audio_bitrate < target_total_bitrate:
             audio_bitrate = min_audio_bitrate
         elif audio_bitrate > max_audio_bitrate:
             audio_bitrate = max_audio_bitrate
-
+    # Target video bitrate, in bps.
     video_bitrate = target_total_bitrate - audio_bitrate
 
-    i = subprocess.Popen(["ffmpeg", "-i", video_full_path, "-c:v", "libx264", "-b:v", str(video_bitrate), "-pass", "1", "-f", "mp4", "-y", "/dev/null"], stderr=subprocess.PIPE)
-    i.communicate()
-    i = subprocess.Popen(["ffmpeg", "-i", video_full_path, "-c:v", "libx264", "-b:v", str(video_bitrate), "-pass", "2", "-c:a", "aac", "-b:a", str(audio_bitrate), "-y", output_file_name], stderr=subprocess.PIPE)
-    i.communicate()
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
+    
+def search_for_folder_path():
+    # Get the list of directories in the current working directory
+    dirs = [d for d in os.listdir('.') if os.path.isdir(d)]
+    # Sort the directories by creation time (newest first)
+    dirs.sort(key=lambda x: os.path.getctime(x), reverse=True)
+    # Return the newest directory
+    if dirs:
+        return os.path.abspath(dirs[0])
 
-# Read the list of files from the text document.
-with open("file_list.txt", "r") as f:
-    file_list = f.readlines()
+def search_for_file_path(folder_path):
+    files = os.listdir(folder_path)
 
-# Download each file in the list using wget.
-for file_url in file_list:
-    file_url = file_url.strip()
-    file_name = file_url.split("/")[-1]
-    download_path = os.path.join(download_directory, file_name)
-    compress_path = os.path.join(compress_directory, file_name.replace(".ts", ".mp4"))
+    # create a list of media file extensions to filter
+    media_ext = ["*.mp4", "*.mkv", "*.avi", "*.wmv"]
 
-    # Download the file using wget.
-    os.system("wget -O {} {}".format(download_path, file_url))
+    # filter the list of files to only include media files
+    media_files = []
+    for ext in media_ext:
+        media_files.extend(fnmatch.filter(files, ext))
 
-    # Compress the file using ffmpeg.
-    compress_video(download_path, compress_path)
+    # get the most recently modified media file
+    latest_media = media_files[0]
 
-    # Remove the downloaded file.
-    os.remove(download_path)
+    # return the name of the most recently modified media file
+    return latest_media
 
-    # Wait for 1 second before processing the next file.
-    time.sleep(1)
 
-# Print a message indicating that all files have been processed.
-print("All files have been processed.")
+def extract(dir_path):
+    rar_files = [f for f in os.listdir(dir_path) if f.endswith('.rar')]
+    rar_files.sort(key=lambda x: os.path.getmtime(os.path.join(dir_path, x)), reverse=True)
+    with rarfile.RarFile(rar_files[0]) as rf:
+    
+        # Print the list of files in the archive
+        print(rf.namelist())
+        
+        # Extract all files to a directory
+        rf.extractall(dir_path)
+
+url = input("Enter the URL of tv series: ")
+dir = input("Enter Directory Name: ")
+x = re.match(r'^(http:|)[/][/]www.([^/]+[.])*todaytvseries2.com', url)
+
+try:
+    if x:
+    
+        req = requests.get(url).content.decode('utf-8')
+        seasons = re.findall(r'Download Season ([0-9]{1,})', req)
+        seasons.sort()
+        get_title = re.search(r'uk-article-title uk-badge1">(.*)</h1>', req).group(1)
+
+        print("\nAvailable seasons to download in" + get_title)
+        for i in seasons:
+            print(get_title + "season"+ " " + i)
+
+        user_input1 = input("\nEnter the season number that you want to download: ").zfill(2)
+        if user_input1 == str("1") or str("01"):
+            g = str(int(user_input1))
+        else:
+            g = str(int(user_input1) - 1)
+        if g in seasons:
+            a = re.findall(r'<div class="cell2">(S'+user_input1+'E[0-9]{1,})', req)
+            print("\nAvailable episodes in " + user_input1 + "\n")
+            print(a)
+            dir_path = "/media"
+            folder_path = "/media/"+dir+"/"
+            for i in a:
+                if i in req:
+                    t = re.findall(r''+i+'</div><div class="cell[0-9]">[0-9]{1,} Mb</div><div class="cell[0-9]"><a href=[\'"]?([^\'" >]+)" class="hvr-icon-sink-away" target="_blank">.*</a></div>', req)[0]
+                    os.system("sudo wget {}".format(t))
+                    rarfolder = search_for_folder_path()
+                    
+                    file_variable = search_for_file_path(dir_path)
+                    if file_variable:
+                        print("Newest file: ", file_variable)
+                        videoin = os.path.join(dir_path+"/"+file_variable)
+                        videoout = os.path.join(folder_path+file_variable.replace('.ts', '.mp4'))
+                        compress_video(videoin, videoout, 50 * 1000)
+                        subprocess.call(['sudo', 'rm', '-r', rarfolder])
+                        
+                    else:
+                        print("No directories found.")
+                else:
+                    print("Unknown Error")
+            
+
+        else:
+            print("Season " + user_input1 + " not available")
+
+    else:
+        print("\nURL not related with todaytvseries2.com domain.")
+
+except KeyboardInterrupt:
+    print('\nProgramme Interrupted')
+  
+print("\nPress any key to exit")
+time.sleep(1)
